dfa0510aa1dd86bf3d4ce442be3f5146e9141e7f
somesh520 <someshtiwari.in@gmail.com>
2025-09-23 09:30:50 -0400

    appchanges

--- Frontend/CyberVidyaCLI/ios/BunkBook/AppDelegate.swift
+++ Frontend/CyberVidyaCLI/ios/BunkBook/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "CyberVidyaCLI",
+      withModuleName: "BunkBook",
       in: window,
       launchOptions: launchOptions
     )
